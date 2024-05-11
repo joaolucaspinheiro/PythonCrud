758932589ce5ae7b5df1c767f75dffe007ba1162
joaolucaspinheiro <lucasdallas25@gmail.com>
2024-05-10 22:14:57 -0300

    Importando a JS, Favicon e Bootstrap

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SobreView
 
 urlpatterns = [
     path('', IndexView.as_view(), name ='index'), #endereço, MinhaView.as_view(), nome da url 
-]
+    path('sobre/', SobreView.as_view(), name ='sobre'), 
+    ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = "paginas/index.html"
+
+class SobreView(TemplateView):
+    template_name = "paginas/sobre.html"
